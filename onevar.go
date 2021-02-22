75c9e8f235d1fb8b66e7920ea76aceb1587198be
CHIRANTH C <chirantc@in.ibm.com>
2021-02-22 16:25:58 +0530

    1st program

---
+++ onevar.go
@@ -0,0 +1,14 @@
+package googlesgobasics
+
+import "fmt"
+
+func onevar() {
+	x := 42
+	y := "James Bond"
+	z := true
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+	fmt.Println("  ")
+	fmt.Println(x, y, z)
+}
